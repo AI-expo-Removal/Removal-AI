e571bcaf8441b349a76a0ae594e83092345a7da7
김대운 <eodnseodns23@gmail.com>
2024-04-18 22:50:32 +0900

    mp4 to mp3

--- video2audio/ffmpeg.py
+++ video2audio/ffmpeg.py
@@ -4,8 +4,8 @@ import os
 
 url = "YOUR_DIR/20240412_230315.mp4" # 나중에 서버에서 받은 url을 사용해야함.
 
-if os.path.exists("YOUR_DIR/testnew.wav"):
-  os.remove("YOUR_DIR/testnew.wav")
-outp = f"YOUR_DIR/testnew.wav" # DB에 POST해야함.
+if os.path.exists("YOUR_DIR/testnew.mp3"):
+  os.remove("YOUR_DIR/testnew.mp3")
+outp = f"YOUR_DIR/testnew.mp3" # DB에 POST해야함.
 
 subprocess.run(["ffmpeg", "-i", url, outp])
