5968ce134ffe435327f9ef3b96da182711e3f7d7
김대운 <eodnseodns23@gmail.com>
2024-04-19 14:19:19 +0900

    📁 :: change ffmpeg file name

--- video2audio/ffmpeg.py
+++ video2audio/ffmpeg.py
@@ -4,8 +4,8 @@ import os
 
 url = "YOUR_FILE_LOCATION" # 나중에 서버에서 받은 url을 사용해야함.
 
-if os.path.exists("YOUR_DIR/testnew.mp3"):
-  os.remove("YOUR_DIR/testnew.mp3")
-outp = f"YOUR_DIR/testnew.mp3" # DB에 POST해야함.
+if os.path.exists("YOUR_DIR/sund.mp3"):
+  os.remove("YOUR_DIR/sund.mp3")
+outp = f"YOUR_DIR/sund.mp3" # DB에 POST해야함.
 
 subprocess.run(["ffmpeg", "-i", url, outp])
