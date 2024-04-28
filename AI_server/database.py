3e9eae3c1b0f077e0f1b63d97363611f154e7e5e
asedish07 <128368531+asedish07@users.noreply.github.com>
2024-04-28 22:27:36 +0900

    Update database.py

--- AI_server/database.py
+++ AI_server/database.py
@@ -1,19 +1,19 @@
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import *
+# from sqlalchemy.orm import sessionmaker
 
-DB_URL = 'mysql+pymysql://root:Aa12345678!@3.36.127.22:3306/removal'
-engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# DB_URL = 'mysql+pymysql://BASE_URL'
+# engine = create_engine(DB_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class engineconn:
-    def __init__(self):
-        self.engine = create_engine(DB_URL, pool_recycle = 500)
+# class engineconn:
+#     def __init__(self):
+#         self.engine = create_engine(DB_URL, pool_recycle = 500)
 
-    def sessionmaker(self):
-        Session = sessionmaker(bind=self.engine)
-        session = Session()
-        return session
+#     def sessionmaker(self):
+#         Session = sessionmaker(bind=self.engine)
+#         session = Session()
+#         return session
 
-    def connection(self):
-        conn = self.engine.connect()
-        return conn
+#     def connection(self):
+#         conn = self.engine.connect()
+#         return conn
